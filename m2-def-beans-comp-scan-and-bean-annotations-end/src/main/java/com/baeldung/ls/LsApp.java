6ab1c6846856bebf8ff6e173e5478a9d0b9ce9c0
Richner Philipp <Philipp.Richner@swisscom.com>
2020-06-22 10:35:22 +0200

    Finished Module 2

---
+++ m2-def-beans-comp-scan-and-bean-annotations-end/src/main/java/com/baeldung/ls/LsApp.java
@@ -0,0 +1,20 @@
+package com.baeldung.ls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.baeldung.ls.config.AppConfig;
+
+@SpringBootApplication(scanBasePackages = "com.baeldung.ls.persistence")
+public class LsApp {
+
+    public static void main(final String... args) {
+        SpringApplication.run(LsApp.class, args);
+        
+        ConfigurableApplicationContext context = SpringApplication.run(new Class[] {LsApp.class, AppConfig.class},args);
+        context.close();
+        
+    }
+
+}
